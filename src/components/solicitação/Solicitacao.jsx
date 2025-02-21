55edde41e0ba65e69d3889df009ffafcb9ed1ef9
vjuniosilva <vjuniosilva@gmail.com>
2025-02-21 12:53:27 -0300

    Estilização do grupo 2 da Solicitação

--- src/components/solicitação/Solicitacao.jsx
+++ src/components/solicitação/Solicitacao.jsx
@@ -7,74 +7,133 @@ import Delete from "../../assets/Solicitacao/deletar.png"
 import styles from "./Solicitacao.module.scss"
 import NavBar from "../navBar/NavBar.jsx"
 
-function Solicitacao(){
+function Solicitacao() {
 
     //const navigate = useNavigate()
 
 
-    return(
+    return (
         <div className={styles.bodySolicitacao}>
-        <NavBar/>
+            <NavBar />
 
-<div className={styles.principalSolicitacao}>
-        <header className={styles.headerSolicitacao}>
-                <img src={Home} alt="Casinha da header" />
-                <img src={Seta} alt="Setinha da header" />
-                <p>Reembolsos</p>
-                <img src={Seta} alt="Setinha da header" />
-                <p>Solicitação de Reembolso</p>
-        </header>
+            <div className={styles.principalSolicitacao}>
+                <header className={styles.headerSolicitacao}>
+                    <img src={Home} alt="Casinha da header" />
+                    <img src={Seta} alt="Setinha da header" />
+                    <p>Reembolsos</p>
+                    <img src={Seta} alt="Setinha da header" />
+                    <p>Solicitação de Reembolso</p>
+                </header>
 
-        <main className={styles.mainSolicitacao}>
-            <form className={styles.formSolicitacao}>
-                <div className={styles.grupo1}>
-                    <div className={styles.inputNome}>
-                        <label htmlFor="">Nome completo</label>
-                        <input type="text" />
-                    </div>
+                <main className={styles.mainSolicitacao}>
+                    <form className={styles.formSolicitacao}>
+                        <div className={styles.grupo1}>
+                            <div className={styles.inputNome}>
+                                <label htmlFor="">Nome completo</label>
+                                <input type="text" />
+                            </div>
 
-                    <div className={styles.inputEmpresa}>
-                        <label htmlFor="">Empresa</label>
-                        <input type="text" />
-                    </div>
+                            <div className={styles.inputEmpresa}>
+                                <label htmlFor="">Empresa</label>
+                                <input type="text" />
+                            </div>
 
-                    <div className={styles.inputPrestacao}>
-                        <label htmlFor="">N° Prest. Contas</label>
-                        <input type="text" />
-                    </div>
+                            <div className={styles.inputPrestacao}>
+                                <label htmlFor="">N° Prest. Contas</label>
+                                <input type="number" />
+                            </div>
 
-                    <div className={styles.inputMotivo}>
-                        <label htmlFor="">Descrição / Motivo do Reembolso</label>
-                        <textarea name="" id=""></textarea>
-                    </div>
-                
-                </div>
+                            <div className={styles.inputMotivo}>
+                                <label htmlFor="">Descrição / Motivo do Reembolso</label>
+                                <textarea name="" id=""></textarea>
+                            </div>
 
-                <div className={styles.barraVertical}>
+                        </div>
 
-                </div>
+                        <div className={styles.barraVertical}>
 
-                <div className={styles.grupo2}>
-                    <div className={styles.inputData}>
-                    <label htmlFor="">Data</label>
-                    <input type="date" />
-                </div>
+                        </div>
 
-                <div className={styles.inputDespesas}>
-                    <select name="" id=""></select>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                </div>
+                        <div className={styles.grupo2}>
+                            <div className={styles.inputData}>
+                                <label htmlFor="">Data</label>
+                                <input type="date" />
+                            </div>
 
-                </div>
-            </form>
+                            <div className={styles.inputTiposDespesas}>
+                                <label htmlFor="">Tipo de Despesa</label>
+                                <select id="">
+                                    <option value=""></option>
+                                    <option value="alimentacao">Alimentação</option>
+                                    <option value="combustivel">Combustível</option>
+                                    <option value="conducao">Condução</option>
+                                    <option value="estacionamento">Estacionamento</option>
+                                    <option value="viagem_admin">Viagem admin.</option>
+                                    <option value="viagem_operacional">Viagem operacional</option>
+                                    <option value="eventos">Eventos de representação</option>
+                                </select>
+                            </div>
 
-        </main>
-        </div>
+                            <div className={styles.inputCusto}>
+                                <label htmlFor="">Centro de Custo</label>
+                                <select id="">
+                                    <option value=""></option>
+                                    <option value="">1100109002 - FIN CONTROLES INTERNOS MTZ</option>
+                                    <option value="">1100110002 - FIN VICE-PRESIDENCIA FINANCAS MTZ</option>
+                                    <option value="">1100110102 - FIN CONTABILIDADE MTZ</option>
+
+                                </select>
+                            </div>
+
+                            <div className={styles.inputOrdem}>
+                                <label htmlFor="">Ord. Int.</label>
+                                <input type="number" />
+                            </div>
+
+                            <div className={styles.inputDiv}>
+                                <label htmlFor="">Div</label>
+                                <input type="number" />
+                            </div>
+
+                            <div className={styles.inputPep}>
+                                <label htmlFor="">PEP</label>
+                                <input type="number" />
+                            </div>
+
+                            <div className={styles.inputMoeda}>
+                                <label htmlFor="">Moeda</label>
+                                <input type="number" />
+                            </div>
+
+                            <div className={styles.inputDistKm}>
+                                <label htmlFor="">Dist/Km</label>
+                                <input type="number" />
+                            </div>
+
+                            <div className={styles.inputValorKm}>
+                                <label htmlFor="">Valor/Km</label>
+                                <input type="number" />
+                            </div>
+
+                            <div className={styles.inputValorFaturado}>
+                                <label htmlFor="">Val. Faturado</label>
+                                <input type="number" />
+                            </div>
+
+                            <div className={styles.inputDespesa}>
+                                <label htmlFor="">Despesa</label>
+                                <input type="number" />
+                            </div>
+
+                            <button className={styles.salvar}>Entrar</button>
+                            <button className={styles.apagar}></button>
+
+                        </div>
+
+                    </form>
+
+                </main>
+            </div>
 
         </div>
     )
